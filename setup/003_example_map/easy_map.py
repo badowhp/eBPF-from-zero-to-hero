95781f084674392fd4a988482c951a8bacd6ef87
Hipolit Badowski <hipolit.badowski@karriere.at>
2024-02-27 01:17:09 +0100

    fix: fixed code

--- setup/003_example_map/easy_map.py
+++ setup/003_example_map/easy_map.py
@@ -4,38 +4,35 @@ from bcc.utils import printb
 from bcc.syscall import syscall_name, syscalls
 from ctypes import *
 
+b = BPF(text="""
 
-b = BPF(text = """
+BPF_HASH(start, u32, u64);
 
-    BPF_HASH(start, u32, u64);
+TRACEPOINT_PROBE(raw_syscalls, sys_exit)
+{
+    u32 syscall_id = args->id;
+    u32 key = 1;
+    u64 *val;
+    u32 uid = bpf_get_current_uid_gid();
 
-
-    TRACEPOINT_PROBE(raw_syscalls, sys_exit)
+    if (uid == 0)
     {
-        u32 syscall_id = args->id;
-        u32 key = 1;
-        u64 *val;
-        u32 uid = bpf_get_current_uid_gid();
+        val = start.lookup(&key); //find value associated with key 1
+        if (val)
+            bpf_trace_printk("Hello world, I have value %d!\\n", *val);
 
-        if (uid == 0)
-        {
-            val = start.lookup(&key); //find value associated with key 1
-            if (val)
-                bpf_trace_printk("Hello world, I have value %d!\\n", *val);
-
-        }
-        return 0;
     }
+    return 0;
+}
 """)
 
-    thisStart = b["start"]
-    thisStart[c_int(1)] = c_int(9) #insert key-value part 1->9
+thisStart = b["start"]
+thisStart[c_int(1)] = c_int(9)  # insert key-value part 1->9
 
-
-    while 1:
-        try:
-            (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-        except KeyboardInterrupt:
-            print("Detaching")
-            exit()
-        print("%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
+while 1:
+    try:
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+    except KeyboardInterrupt:
+        print("Detaching")
+        exit()
+    print("%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
